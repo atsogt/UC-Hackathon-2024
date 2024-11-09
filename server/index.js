82373cd6985d23ffbd1572bfa08722d1fa924665
atsogt <atsogt17@gmail.com>
2024-11-09 18:10:08 -0500

    server is setup with database connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,39 @@
 const express = require('express');
 require("dotenv").config();
 const cors = require("cors");
+const mongoose = require('mongoose');
+// const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoURI = process.env.MONGO_URI;
+const Student = require('./models/Students');
+
+const newStudent = new Student({
+  name: 'John Doe',
+  email: 'johndoe@example.com',
+  password: 'your_strong_password',
+  isAdmin: false,
+  interests: ['coding', 'gaming', 'reading']
+});
+
+newStudent.save()
+  .then(savedStudent => {
+    console.log('Student saved successfully:', savedStudent);
+  })
+  .catch(err => {
+    console.error('Error saving student:', err);
+  });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 3001 || process.env.PORT;
+
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB Atlas:', err);
+  });
 
 app.get('/', (req, res) => {
   res.send("Hello from hackathon UC")
---
+++ server/models/Students.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  interests: {
+    type: [String],
+    default: []
+  }
+});
+
+module.exports = mongoose.model('Student', studentSchema);
